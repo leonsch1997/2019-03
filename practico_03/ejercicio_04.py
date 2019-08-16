a370cb98ed22a7b2608e3db3674d4a92f08aa483
alesoftware <alemail@usa.net>
2019-08-15 23:03:28 -0300

    Update ejercicio_04.py

--- practico_03/ejercicio_04.py
+++ practico_03/ejercicio_04.py
@@ -4,19 +4,31 @@
 
 import datetime
 
-from practico_03.ejercicio_01 import reset_tabla
+from practico_03.ejercicio_01 import reset_tabla, crear_conexion
 from practico_03.ejercicio_02 import agregar_persona
 
 
 def buscar_persona(id_persona):
-    return False
+    cSQL = """SELECT IdPersona, Nombre, FechaNacimiento, Dni, Altura 
+              FROM Persona 
+              WHERE (IdPersona = ?)"""
+    dato = (id_persona,)
+    with crear_conexion() as db:
+        cursor = db.cursor()
+        exists = cursor.execute(cSQL, dato).fetchone()
+
+        if exists is None:
+            return False
+        else:
+            return exists
 
 
 @reset_tabla
 def pruebas():
     juan = buscar_persona(agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180))
-    assert juan == (1, 'juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
+    assert juan == (1, 'juan perez', str(datetime.datetime(1988, 5, 15)), 32165498, 180)
     assert buscar_persona(12345) is False
 
 if __name__ == '__main__':
     pruebas()
+
