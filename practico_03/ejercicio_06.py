b1e82c4e7145fbde9dd73301f3f509ccb755589a
alesoftware <alemail@usa.net>
2019-08-15 23:04:54 -0300

    Update ejercicio_06.py

--- practico_03/ejercicio_06.py
+++ practico_03/ejercicio_06.py
@@ -5,15 +5,32 @@
 
 # Implementar la funcion borrar_tabla, que borra la tabla creada anteriormente.
 
-from practico_03.ejercicio_01 import borrar_tabla, crear_tabla
+from practico_03.ejercicio_01 import reset_tabla, crear_conexion, crear_tabla, borrar_tabla
 
 
 def crear_tabla_peso():
-    pass
+    cSQL = """CREATE TABLE IF NOT EXISTS 
+               PersonaPeso(
+               idPeso INTEGER PRIMARY KEY, 
+               idPersona INTEGER ,
+               fecha TEXT ,
+               peso INTEGER, 
+               FOREIGN KEY (idPersona) REFERENCES Persona(idPersona)
+               ) """
+
+    with crear_conexion() as db:
+        cursor = db.cursor()
+        cursor.execute(cSQL)
+        db.commit()
 
 
 def borrar_tabla_peso():
-    pass
+    cSQL = 'DROP TABLE IF EXISTS PersonaPeso'
+
+    with crear_conexion() as db:
+        cursor = db.cursor()
+        cursor.execute(cSQL)
+        db.commit()
 
 
 # no modificar
@@ -25,3 +42,4 @@ def reset_tabla(func):
         borrar_tabla_peso()
         borrar_tabla()
     return func_wrapper
+
