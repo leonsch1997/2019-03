0405730f2eb871c660518f2734d2826b0d8334ba
alesoftware <alemail@usa.net>
2019-08-15 23:01:47 -0300

    Update ejercicio_01.py

--- practico_03/ejercicio_01.py
+++ practico_03/ejercicio_01.py
@@ -1,8 +1,3 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
 # Implementar la funcion crear_tabla, que cree una tabla Persona con:
 # - IdPersona: Int() (autoincremental)
 # - Nombre: Char(30)
@@ -11,30 +6,47 @@ from sqlalchemy.orm import sessionmaker
 # - Altura: Int()
 
 # Implementar la funcion borrar_tabla, que borra la tabla creada anteriormente.
+import sqlite3
 
-Base = declarative_base() # Metadatos
-engine=create_engine('sqlite:///sqlalchemy_ejemplo0.db')
-Base.metadata.bind=engine
-
-#---- creamos una sesión para admin datos
-DBSession = sessionmaker()
-DBSession.bind = engine
-session = DBSession()
+def crear_conexion():
+    return sqlite3.connect('base.db')
 
 def crear_tabla():
-    class Persona(Base):
-        __tablename__='persona'
-        IdPersona = Column(Integer, primary_key=True)
-        Nombre = Column(String(30), nullable=False)
-        FechaNacimiento= Column(String(30), nullable=False)
-        DNI=Column(Integer, nullable=False)
-        Altura=Column(Integer)
-    Base.metadata.create_all(engine)
 
+    cSQL = """
+      CREATE TABLE IF NOT EXISTS 
+      Persona(
+          IdPersona INTEGER PRIMARY KEY,
+          Nombre TEXT(25),
+          FechaNacimiento TEXT,
+          Dni INTEGER,
+          Altura INTEGER
+        )
+    """
+
+    with crear_conexion() as db:
+        cursor = db.cursor()
+        cursor.execute(cSQL)
+        db.commit()
 
 
 def borrar_tabla():
-    Base.metadata.delete_table('Persona')
+    cSQL = 'DROP TABLE IF EXISTS Persona'
+
+    with crear_conexion() as db:
+        cursor = db.cursor()
+        cursor.execute(cSQL)
+        db.commit()
+
+# no modificar
+def reset_tabla(func):
+    def func_wrapper():
+        crear_tabla()
+        func()
+        borrar_tabla()
+    return func_wrapper
+
+
     
 
 
